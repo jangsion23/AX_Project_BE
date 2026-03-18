ba0cbd0ef6e32836ce9905925a2bed843df7a99e
jangsion23 <sionjang23@naver.com>
2026-03-18 21:00:54 +0900

    modify : DTO 파트 lombok 및 Ai 수정

--- src/main/java/com/knuaf/chickenstock/dto/SignInDto.java
+++ src/main/java/com/knuaf/chickenstock/dto/SignInDto.java
@@ -1,10 +1,12 @@
 package com.knuaf.chickenstock.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignInDto {
@@ -12,19 +14,4 @@ public class SignInDto {
     private String id;
     private String password;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/com/knuaf/chickenstock/dto/SignUpDto.java
+++ src/main/java/com/knuaf/chickenstock/dto/SignUpDto.java
@@ -1,9 +1,15 @@
 package com.knuaf.chickenstock.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.*;
 
-@Data
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
